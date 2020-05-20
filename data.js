8aee7e7c6240c49e64e4aa6357fdb8ea45cfdbe1
cheonma-director <65658583+cheonma-director@users.noreply.github.com>
2020-05-20 20:18:10 +0900

    VR_view

---
+++ data.js
@@ -0,0 +1,38 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-kakaotalk_20200514_105941517",
+      "name": "KakaoTalk_20200514_105941517",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.640635126931759,
+        "pitch": 0.5662273256540402,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
